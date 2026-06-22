565b9a30cacee0d9c2d380e1596c84d7a947fe19
Nedra Seid <nedraseid@gmail.com>
2026-06-22 18:45:09 +0300

    basic javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+/*console.log("Asselamu aleykum");
+console.log(`u doing good??`);
+window.alert(`This is alert so run run run`);
+window.alert(`so are u happy`);*/
+document.getElementById(`firsth1`).textContent = `Asselamu aleykum`;
+document.getElementById(`firstp`).textContent = `keyfe haluki?`;
