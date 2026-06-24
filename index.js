50a22613b7defb67b48ac816910eccd994f85560
Nedra Seid <nedraseid@gmail.com>
2026-06-24 12:38:18 +0300

    updated javascript

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 /*console.log("Asselamu aleykum");
 console.log(`u doing good??`);
 window.alert(`This is alert so run run run`);
-window.alert(`so are u happy`);*/
+window.alert(`so are u happy`);
 document.getElementById(`firsth1`).textContent = `Asselamu aleykum`;
-document.getElementById(`firstp`).textContent = `keyfe haluki?`;
+document.getElementById(`firstp`).textContent = `keyfe haluki?`;*/
+
+
+
+
+
+
+let age = 11;
+let girlname = `Nedra`;
+let gpa = 3.85;
+let name = `donkey`;
+let email = `asmaabubeker@gmail.com;`
+let isStudent = true;
+let fatherName = `Seid`;
+
+
+console.log(`You are ${age} years old.`);
+console.log(`from animals ${name} is really hard working one`);
+console.log(`your class mate ${girlname} is very clever and even got ${gpa} on first semister`);
+console.log(`You are ${age} years old.`);
+console.log(`is this your email? ${email}`);
+console.log(`is the student enrolled?: ${isStudent}`);
+console.log(`so the girls father name is ${fatherName}`);
