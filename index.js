cc119efb61a3fa27f32ed46b46925b78c9aee2ea
Nedra Seid <nedraseid@gmail.com>
2026-07-27 17:31:28 +0300

    learned about variables

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ console.log(`u doing good??`);
 window.alert(`This is alert so run run run`);
 window.alert(`so are u happy`);
 document.getElementById(`firsth1`).textContent = `Asselamu aleykum`;
-document.getElementById(`firstp`).textContent = `keyfe haluki?`;*/
+document.getElementById(`firstp`).textContent = `keyfe haluki?`;
 
 
 
-
+document.getElementById("firsth1").textContent = `nedra`;
 
 
 let age = 11;
@@ -19,10 +19,39 @@ let isStudent = true;
 let fatherName = `Seid`;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(`You are ${age} years old.`);
 console.log(`from animals ${name} is really hard working one`);
 console.log(`your class mate ${girlname} is very clever and even got ${gpa} on first semister`);
 console.log(`You are ${age} years old.`);
 console.log(`is this your email? ${email}`);
 console.log(`is the student enrolled?: ${isStudent}`);
-console.log(`so the girls father name is ${fatherName}`);
+console.log(`so the girls father name is ${fatherName}`); */
+
+
+
+
+let fullName = "Nedra";
+let age = 19;
+let isStudent = false;
+
+
+
+document.getElementById("firsth1").textContent = `The name ${fullName} is such an awsome name to have.`;
+document.getElementById("firstp").textContent = `the girl named ${fullName} just turned ${age} a month ago.` ;
+document.getElementById("secondp").textContent = ` Is ${fullName} a student?: ${isStudent}`;
