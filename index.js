34e3b5d454922351ff09a895f840dd3c7b03ef14
Nedra Seid <nedraseid@gmail.com>
2026-07-30 16:10:13 +0300

    done more ex on if ese

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const myLabel = document.getElementById("myLabel");
-const myText = document.getElementById("myText");
-const myBtn = document.getElementById("myBtn");
-const myP = document.getElementById("myP");
-let age;
-
-myBtn.onclick = function(){
-    age = myText.value;
-    age = Number(age);
-    if(age >= 80){
-        myP.textContent = "Sorry sir you cannot be alone ur old";
-    }
-    else if(age >= 18){
-       myP.textContent = "you are capable of living by your own";
-    }
-    
-    else if(age == 0){
-        myP.textContent = "you just born kiddo go cry or something";
-    }
-    else if(age <= 10){
-        myP.textContent = "you need a legal guardian";
-    }
-    else if(age < 0){
-        myP.textContent = "age can not be under 0";
-    }
-}
+let time = 4;
+let greetings = time >= 5 ? "good afternoon" : "good morning";
+console.log(greetings);
