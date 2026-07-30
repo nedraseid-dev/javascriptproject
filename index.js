99132d118ffa6b0b2b086793789aef45fb3be644
Nedra Seid <nedraseid@gmail.com>
2026-07-30 17:15:21 +0300

    practiced switch

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-let purchaseAmount = 120;
-let discount = purchaseAmount >= 100 ? 100 : 0;
-console.log(`your total is $${purchaseAmount - (discount/100)}`)
+let name = "yusra";
+
+switch(name){
+    case 1:
+        console.log("My child name is Abdellah");
+        break;
+    case 2:
+        console.log("My child name is Hanifa");
+        break;
+    case 3:
+        console.log("My child name is Nedra");
+        break;
+    case 4:
+        console.log("My child name is Abdurahman");
+        break;
+    case 5:
+        console.log("My child name is Sumeya");
+        break;
+    case 6:
+        console.log("My child name is Halima")
+        break;
+    case 7:
+        console.log("My child name is Mohammed")    
+        break;
+    case 8:
+        console.log("My child name is Musab")
+        break;
+    default:
+        console.log(`${name} is not my child`)
+
+
+
+}
