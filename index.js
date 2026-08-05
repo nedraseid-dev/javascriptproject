bcc334531f79285278db55df21dd4cfc5f9e324a
Nedra Seid <nedraseid@gmail.com>
2026-08-05 18:23:55 +0300

    LEARNT WHILE LOOP IN JAVASCRIPT

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const PI = 3.14;
-if(PI === "3.14"){
-    console.log("That is pi");
-} 
-else{
-    console.log("That is not pi");
+let username = "NEDRA";
+while(username === ""){
+    console.log("you did not entered username")
 }
+console.log(`your name is ${username}`);
