a30422fb997a4dab0137c161d8b3c1938eb0b9c2
Nedra Seid <nedraseid@gmail.com>
2026-08-05 20:28:12 +0300

    learnt do while loop

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let username = "NEDRA";
-while(username === ""){
-    console.log("you did not entered username")
-}
-console.log(`your name is ${username}`);
+let loggedIn = false;
+let username;
+let password;
+
+do{
+    username = window.prompt(`enter your name`);
+    password = window.prompt(`enter your password`);
+    
+    if(username === "myusername" && password === "mypassword"){
+     loggedIn = true;
+     console.log(`u are in`);
+    }
+    else{
+        console.log(`Invalid credentials please try again`);
+
+    }
+
+}while(!loggedIn)
