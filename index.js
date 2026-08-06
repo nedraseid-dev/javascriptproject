cd2de9cb94a722271e78f72eee748a2e13fd071a
Nedra Seid <nedraseid@gmail.com>
2026-08-06 16:55:55 +0300

    learned birsthday function

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
- 
-let attempts = 0;
-let guess;
-let running = true;
+function happyBirthday(username, age){
+    console.log("Happy Birthday to you!")
+    console.log("Happy Birthday to you!")
+    console.log(`Happy Birthday dear ${username}!`)
+    console.log("Happy Birthday to you!")
+    console.log(`you are now ${age} years old!`)
 
-while(running){
-    guess = window.prompt(`Guess a number from ${minNum} to ${maxNum}:`);
-    console.log(typeof guess, guess);
-
-    if(isNaN(guess)){
-        window.alert(`please enter a valid number!`);
-    }
-    else if(guess > maxNum || guess < minNum){
-        window.alert(`please enter a valid number!`);
-    }
-    else{
-        attempts++;
-        if(guess < answer){
-            window.alert("that number is too low!  try again!");
-        }
-        else if(guess > answer){
-            window.alert("that number is too high! try again!");
-        }
-        else{
-            window.alert(`perfect! the answer was ${answer} it took u ${attempts} attempts `)
-            running = false;
-        }
-    }    
 }
-
-    
-
-
-
-
-
-
-
-
-
-
+happyBirthday("Nedra" , 19);
+happyBirthday("Amira" , 20);
