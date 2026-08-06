6d7a7cd2979bc8ff48ee9a39c853167434c159b1
Nedra Seid <nedraseid@gmail.com>
2026-08-06 15:11:15 +0300

    learnt for loop

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let loggedIn = false;
-let username;
-let password;
-
-do{
-    username = window.prompt(`enter your name`);
-    password = window.prompt(`enter your password`);
-    
-    if(username === "myusername" && password === "mypassword"){
-     loggedIn = true;
-     console.log(`u are in`);
-    }
-    else{
-        console.log(`Invalid credentials please try again`);
-
-    }
-
-}while(!loggedIn)
+for(let i = 10; i >= 0; i-=3){
+    console.log(i);
+}
