3c8e491e3922e93bc2e011f78745b69e7c4737ad
Nedra Seid <nedraseid@gmail.com>
2026-08-06 16:33:16 +0300

    done a number guesssing game

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-for(i = 1; i <=30; i++){
-    if(i == 21){
-        break;
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+ 
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a number from ${minNum} to ${maxNum}:`);
+    console.log(typeof guess, guess);
+
+    if(isNaN(guess)){
+        window.alert(`please enter a valid number!`);
+    }
+    else if(guess > maxNum || guess < minNum){
+        window.alert(`please enter a valid number!`);
     }
     else{
-        console.log(i);
-    }
+        attempts++;
+        if(guess < answer){
+            window.alert("that number is too low!  try again!");
+        }
+        else if(guess > answer){
+            window.alert("that number is too high! try again!");
+        }
+        else{
+            window.alert(`perfect! the answer was ${answer} it took u ${attempts} attempts `)
+            running = false;
+        }
+    }    
 }
+
+    
+
+
+
+
+
+
+
+
+
+
