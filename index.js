c38753fe5152533596ceec67393e9722526319e7
Nedra Seid <nedraseid@gmail.com>
2026-08-07 20:57:35 +0300

    done ex on variabes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function2();
-function function1(){
-    let x = 1;
-    console.log(x);
-}
-function function2(){
-    let x = 2;
-    console.log(x);
+let fruits = ["Cocunant", "Apple", "Banana", "Guava"]
+
+fruits.sort().reverse();
+
+for(let fruit of fruits){
+    console.log(fruit);
 
 }
+
+
+
