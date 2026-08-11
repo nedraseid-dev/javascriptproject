b6b6d06d0f8d0d0e261b03ac2f293f2df7122290
Nedra Seid <nedraseid@gmail.com>
2026-08-11 11:59:14 +0300

    learnt spread operator

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const matrix = [[1, 2, 3], 
-               [4, 5, 6],
-               [7, 8, 9]];
-
-matrix[0][0] = 'x';    
-matrix[0][1] = 'y';               
-matrix[0][2] = 'z';               
-
-
-
-for(let row of matrix){
-    const rowString = row.join(' ');
-console.log(rowString);
-}
+let fruits = ["apple", "banana", "orange"];
+let vegtables = ["tomato", "chillie", "cabbage"]
+let food = [...fruits, ...vegtables];
+console.log(food);
