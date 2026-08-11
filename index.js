b0a73d6fd986b96b55f569e65dd412eef9b59f3c
Nedra Seid <nedraseid@gmail.com>
2026-08-11 10:30:24 +0300

    learnt 2 d arrays

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let fruits = ["Cocunant", "Apple", "Banana", "Guava"]
+const matrix = [[1, 2, 3], 
+               [4, 5, 6],
+               [7, 8, 9]];
 
-fruits.sort().reverse();
+matrix[0][0] = 'x';    
+matrix[0][1] = 'y';               
+matrix[0][2] = 'z';               
 
-for(let fruit of fruits){
-    console.log(fruit);
 
+
+for(let row of matrix){
+    const rowString = row.join(' ');
+console.log(rowString);
 }
-
-
-
