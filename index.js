d2273c6b8d2a6ee2301998806471231450644a10
Nedra Seid <nedraseid@gmail.com>
2026-08-15 12:40:04 +0300

    fridgr containers

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let fruits = ["apple", "banana", "orange"];
-let vegtables = ["tomato", "chillie", "cabbage"]
-let food = [...fruits, ...vegtables];
-console.log(food);
+function openFridge(...foods){
+    console.log(...foods);
+}
+function getFood(...foods){
+    return foods;
+}
+
+const food1 = "enjera";
+const food2 = "dabo";
+const food3 = "kinche";
+const food4 = "arusto";
+
+//openFridge(food1, food2, food3, food4);
+
+const foods = gretFood(food1, food2, food3, food4);    
