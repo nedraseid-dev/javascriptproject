2d4dea3e8c21eacb9111656a5a13549026288b9c
Nedra Seid <nedraseid@gmail.com>
2026-08-18 16:00:24 +0300

    practiced the rest perametrs

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-function sum(...numbers){
+function combineStrings(...strings){
+    return strings.join(" ");
 
-    let result = 0;
-    for(let number of numbers){
-        result += number; 
-    }
-    return result;
 }
-
-function getAverage(...numbers){
-
-    let result = 0;
-    for(let number of numbers){
-        result += number; 
-    }
-    return result / numbers.length;
-}
-const total = getAverage(75, 100, 85, 35, 65);
-console.log(total);
+const fullName = combineStrings("Ms.", "Nedra", "Seid", "111")
+console.log(fullName);
